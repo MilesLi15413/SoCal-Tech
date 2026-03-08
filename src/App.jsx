ac2ec5534f41b980b567a527c66d3e4046d12c67
MilesLi15413 <milesli15413@gmail.com>
2026-03-08 13:56:32 -0700

    initial commite

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className= "background-section">
+      <h1> Building Community Through Technology</h1>  
+      <p1>As a 501(c)(3) nonprofit, SoCal Tech Forum strives to cultivate an environment where networking, learning, and sharing ideas drive the growth of technology and its impact on daily life. </p1>    
+    </div>
+  )
+}
+
+export default App
