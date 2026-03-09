6f4b48ea9286b8b6fd0bddcf0e00d4cdfa1baa6d
MilesLi15413 <milesli15413@gmail.com>
2026-03-08 18:50:29 -0700

    title and subtitle font and light gray background

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import './App.css'
 function App() {
   return (
     <div className= "background-section">
-      <h1> Building Community Through Technology</h1>  
-      <p1>As a 501(c)(3) nonprofit, SoCal Tech Forum strives to cultivate an environment where networking, learning, and sharing ideas drive the growth of technology and its impact on daily life. </p1>    
+      <div className= "content-wrapper"> 
+      <h1 className= "content-title"> Building Community Through Technology</h1>  
+      <p className= "content-sub-title">As a 501(c)(3) nonprofit, SoCal Tech Forum strives to cultivate an environment where networking, learning, and sharing ideas drive the growth of technology and its impact on daily life. </p>    
+     </div>
     </div>
   )
 }
