f9260d10592882a1bc7eb2462a011b1c7c333448
MilesLi15413 <milesli15413@gmail.com>
2026-03-13 21:22:50 -0700

    trying to get the two images to display

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ function App() {
   return (
     <div className= "background-section">
       <div className= "content-wrapper"> 
-      <h1 className= "content-title"> Building Community Through Technology</h1>  
-      <p className= "content-sub-title">As a 501(c)(3) nonprofit, SoCal Tech Forum strives to cultivate an environment where networking, learning, and sharing ideas drive the growth of technology and its impact on daily life. </p>    
+        <h1 className= "content-title"> Building Community Through Technology</h1>  
+        <p className= "content-sub-title">As a 501(c)(3) nonprofit, SoCal Tech Forum strives to cultivate an environment where networking, learning, and sharing ideas drive the growth of technology and its impact on daily life. </p>    
+      </div>
+     <div className= "photo-circle">
+       <img src= "../Elements/Chris.jpg" />
      </div>
+     <div className= "photo-circle-ppl">
+       <img src= "../Elements/SoCal.jpg" />
+     </div>
+     
     </div>
   )
 }
